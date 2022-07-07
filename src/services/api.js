36cb22ea157edf1fec9bd9f7822a108c70cf418f
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2022-07-07 17:18:35 -0300

    iniciando o projeto

--- src/services/api.js
+++ src/services/api.js
@@ -8,4 +8,5 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`);
   const responseJson = await response.json();
   return responseJson;
+  
 }
