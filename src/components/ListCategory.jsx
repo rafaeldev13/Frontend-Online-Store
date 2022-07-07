f12614dbb6890aa9bb2b0c09fdfe7ef48530f11d
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-07-07 17:02:00 -0400

    Iniciando Requisito 04

---
+++ src/components/ListCategory.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ListaCategory extends Component {
+  render() {
+    return (
+      <p
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+    );
+  }
+}
+
+export default ListaCategory;
