ad143f234e29b7675fe46b6e5a1a28cbcc011251
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-07-07 17:39:15 -0400

    Requisito 05

--- src/components/ListCategory.jsx
+++ src/components/ListCategory.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class ListaCategory extends Component {
-  componentDidMount() {
-    
+  constructor() {
+    super();
+    this.state = {
+      categoryList: [],
+    };
   }
+
+  async componentDidMount() {
+    this.setState({
+      categoryList: await getCategories(),
+    });
+  }
+
   render() {
+    const { categoryList } = this.state;
     return (
       <aside>
-        
+        {
+          categoryList.map((category) => (
+            <label
+              key={ category.id }
+              htmlFor={ `categoria${category.id}` }
+              data-testid="category"
+            >
+              { category.name }
+              <input
+                type="radio"
+                id={ `categoria${category.id}` }
+              />
+            </label>))
+        }
       </aside>
     );
   }
